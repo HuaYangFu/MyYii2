dd0a651e4789ea6e06251999f37f528f132d69e6
華暘 Jacky <ms2023645@gmail.com>
2017-12-28 00:44:20 +0800

    sql

--- sql/testdata/testsql1data.sql
+++ sql/testdata/testsql1data.sql
@@ -1,17 +1,21 @@
 insert
-into userdb.jackytest(label)
-values ('分類一');
+into userdb.jackytest(id,label)
+values (1,'分類一');
 
 insert
-into userdb.jackytest(label)
-values ('分類一');
+into userdb.jackytest(id,label)
+values (2,'分類一');
 
 insert
-into userdb.jackytest(label)
-values ('分類二');
+into userdb.jackytest(id,label)
+values (5,'分類一');
 
 insert
-into userdb.jackytest(label)
-values ('分類三');
+into userdb.jackytest(id,label)
+values (3,'分類二');
+
+insert
+into userdb.jackytest(id,label)
+values (4,'分類三');
 
 
--- sql/testdata/testsql2data.sql
+++ sql/testdata/testsql2data.sql
@@ -1,17 +1,21 @@
 insert
-into userdb.jackytest2(postdate, no)
-values ('20150101',1);
+into userdb.jackytest2(id,postdate, no)
+values (1,'20150101',1);
 
 insert
-into userdb.jackytest2(postdate, no)
-values ('20150201',2);
+into userdb.jackytest2(id,postdate, no)
+values (2,'20150101',2);
 
 insert
-into userdb.jackytest2(postdate, no)
-values ('20160101',3);
+into userdb.jackytest2(id,postdate, no)
+values (5,'20150201',5);
 
 insert
-into userdb.jackytest2(postdate, no)
-values ('20170101',4);
+into userdb.jackytest2(id,postdate, no)
+values (3,'20160101',3);
+
+insert
+into userdb.jackytest2(id,postdate, no)
+values (4,'20170101',4);
 
 
